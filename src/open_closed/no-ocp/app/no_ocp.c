7f4e6acc745368a9fd7d04a210bd787b084f3b09
André B. M. Gomes <bmg.andre@gmail.com>
2019-02-16 23:01:04 -0200

    [no-ocp] refactor main function

--- src/open_closed/no-ocp/app/no_ocp.c
+++ src/open_closed/no-ocp/app/no_ocp.c
@@ -3,6 +3,13 @@
 #include "../financial_report/financial_colored_console_reporter.h"
 #include <stdio.h>
 
+typedef enum report_type {
+    BLACK_WHITE,
+    COLORED
+} report_type;
+
+static report_type get_report_type_from_user(void);
+
 int main() {
     struct financial_report_generator * financial_report_generator = new_financial_report_generator();
 
@@ -17,13 +24,9 @@ int main() {
 
     struct financial_report_result * financial_report_result = financial_report_generator->get_transactions(&start_date, &end_date);
 
-    char colored_output = '\0';
-    do {
-        printf("Colored output (Y/N)? ");
-        scanf(" %c", &colored_output);
-    } while (colored_output != 'Y' && colored_output != 'N');
+    report_type report_type = get_report_type_from_user();
 
-    if (colored_output == 'Y') {
+    if (report_type == COLORED) {
         struct financial_colored_console_reporter * colored_reporter = new_financial_colored_console_reporter();
         colored_reporter->print(financial_report_result->result);
         free_financial_colored_console_reporter(colored_reporter);
@@ -37,4 +40,16 @@ int main() {
     free_financial_report_generator(financial_report_generator);
 
     return 0;
+}
+
+static report_type get_report_type_from_user() {
+    char colored_output = '\0';
+    do {
+        printf("Colored output (Y/N)? ");
+        scanf(" %c", &colored_output);
+    } while (colored_output != 'Y' && colored_output != 'N');
+
+    report_type report_type = colored_output == 'Y' ? COLORED : BLACK_WHITE;
+
+    return report_type;
 }
